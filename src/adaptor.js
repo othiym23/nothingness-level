14d4cc9b3927c6fd23c08726701f7b4d86d1a2eb
Forrest L Norvell <forrest@npmjs.com>
2016-05-22 22:56:55 -0700

    dao: optionally pass in Symbol to use to set ID
    There's nothing saying that it needs to be a Symbol, but you're going to
    experience some very confusing results if you use an enumerable name for
    this and don't strip it off properly at save time.

--- src/adaptor.js
+++ src/adaptor.js
@@ -3,8 +3,11 @@ import DAO from 'nothingness'
 
 // the dumbest possible leveldb adaptor
 export default class Adaptor {
-  constructor (path) {
+  // need to be able to pass the idSymbol in cases where there could be two
+  // instances of the DAO class floating around (i.e. when testing)
+  constructor (path, idSymbol = DAO.idSymbol) {
     this.db = level(path, { valueEncoding: 'json' })
+    this.idSymbol = idSymbol
   }
 
   save (key, value, cb) {
@@ -13,10 +16,11 @@ export default class Adaptor {
 
   findAll (cb) {
     var results = []
+    var idSymbol = this.idSymbol
     this.db
       .createReadStream()
       .on('data', function (entry) {
-        entry.value[DAO.idSymbol] = entry.key
+        entry.value[idSymbol] = entry.key
         results.push(entry.value)
       })
       .on('end', function () { cb(null, results) })
